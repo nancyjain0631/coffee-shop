6aed087ecdacb4407aed4541cc1c9474489e425a
Nancy Jain <nancyjain0631@gmail.com>
2020-05-18 12:40:12 +0530

    drink section

--- js/app.js
+++ js/app.js
@@ -19,6 +19,26 @@ function eventListeners() {
         ui.videoControls();
 
     })
+    // submit the form
+    document.querySelector('.drink-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const name = document.querySelector('.input-name').value;
+        const lastname = document.querySelector('.input-lastname').value;
+        const email = document.querySelector('.input-email').value;
+        let value = ui.checkEmpty(name, lastname, email)
+
+        if (value) {
+            //if value is true
+            let customer = new Customer(name, lastname, email)
+            console.log(customer);
+            ui.addCustomer(customer)
+            ui.showfeedback('customer added to the list', 'success')
+            ui.clearFields()
+
+        } else { //if value is false
+            ui.showfeedback('some form values empty', 'error')
+        }
+    })
 }
 
 //constructor function
@@ -43,4 +63,69 @@ UI.prototype.videoControls = function () {
         btn.classList.remove('btnSlide')
         document.querySelector('.video_item').play();
     }
+}
+//check for empty value in form
+UI.prototype.checkEmpty = function (name, lastname, email) {
+    let result;
+    if (name === '' || lastname === '' || email === '') {
+        result = false;
+    } else {
+        result = true;
+    }
+    return result;
+}
+UI.prototype.showfeedback = function (text, type) {
+    const feedback = document.querySelector('.drink-form_feedback');
+    if (type === "success") {
+
+        feedback.classList.add('success');
+        feedback.innerText = text;
+        this.removeAlert('success');
+    } else if (type === 'error') {
+        let feedback = document.querySelector('.drink-form_feedback');
+        feedback.classList.add('error');
+        feedback.innerText = text;
+        this.removeAlert('error');
+    }
+}
+//remove alert
+UI.prototype.removeAlert = function (type) {
+    setTimeout(function () {
+        document.querySelector('.drink-form_feedback').classList.remove(type)
+
+    }, 3000)
+}
+//add customer
+UI.prototype.addCustomer = function (customer) {
+    const images = [1, 2, 3, 4, 5];
+    let random = Math.floor(Math.random() * images.length);
+    const div = document.createElement('div');
+    div.classList.add('person');
+    div.innerHTML = `<img src='./assets/img/person-${random}.png' alt='person' class='person_thumbnail'><h4 class='person_name'> ${customer.name}</h4><h4 class ='person_last-name'>${customer.lastname}</h4>`
+
+    document.querySelector('.drink-card_list').appendChild(div)
+
+}
+// clear function
+UI.prototype.clearFields = function () {
+    document.querySelector('.input-name').value = '';
+    document.querySelector('.input-lastname').value = '';
+    document.querySelector('.input-email').value = '';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function Customer(name, lastname, email) {
+    this.name = name,
+        this.lastname = lastname,
+        this.email = email;
 }
